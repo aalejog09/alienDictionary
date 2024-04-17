f64fabf4e43c6cb873656b7c6eaaeb6f112e1602
Andres Alejo <aalejog09@gmail.com>
2024-04-17 16:39:12 -0400

    first commit base project

---
+++ src/main/java/com/iqt/alienDictionary/alienDictionay/AlienDictionayApplication.java
@@ -0,0 +1,13 @@
+package com.iqt.alienDictionary.alienDictionay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlienDictionayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlienDictionayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iqt/alienDictionary/alienDictionay/AlienDictionayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iqt.alienDictionary.alienDictionay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlienDictionayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
